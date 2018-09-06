85fb296ff60004839e8f292065b99aa29e04568e
fcj1113 <fcj1113@163.com>
2018-09-06 10:49:15 +0800

    modify

---
+++ mss-modules-parent/mss-msgcenter-service/src/main/java/com/miaoshasha/msgcenter/MsgCenterApplication.java
@@ -0,0 +1,25 @@
+package com.miaoshasha.msgcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * 消息中心，主要为短信，微信等消息发送
+ * @author：fengchaojun <br/>
+ * ===============================
+ * Created with IDEA.
+ * Date：2018-09-06
+ * Time：10:16
+ * ================================
+ */
+@EnableEurekaClient
+@ComponentScan("com.miaoshasha")
+@SpringBootApplication
+public class MsgCenterApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(MsgCenterApplication.class,args);
+    }
+}
